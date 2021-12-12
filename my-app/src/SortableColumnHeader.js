653579077b0d50a7eeae5ef6c075ce5db93e7751
nouraliSDSU <nhali@sdsu.edu>
2021-12-12 15:37:54 -0800

    Working code

--- my-app/src/SortableColumnHeader.js
+++ my-app/src/SortableColumnHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './SortableColumnHeader.css';
+
 
 class SortableColumnHeader extends React.Component {
   constructor(props) {
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,51 +1,6 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Products from './Products.js'
 
-let exerciseData = {
-    total: 50,
-    lowerbody: 20,
-    upperBody: 10,
-    cardio: 20,
-    goal: 100
-}
 
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-
-const calculateGoalProgress = (total, goal) => {
-  return getPercent(total /goal)
-}
-
-const ExcerciseDayCounter = ({total, lowerBody, upperBody, cardio, goal}) => {
-    return(
-      <section>
-        <div>
-            <p> Exercise Days: {total} </p>
-        </div>
-        <div>
-            <p> Lower Body Days: {lowerBody} </p>
-        </div>
-        <div>
-            <p> Upper Body Days: {upperBody} </p>
-        </div>
-        <div>
-            <p> Upper Body Days: {cardio} </p>
-        </div>
-        <div>
-            <p> Goal Progress: {calculateGoalProgress(total,goal)} </p>
-        </div>
-      </section>
-    )
-}
-
-render(
-   <ExcerciseDayCounter
-   total = {exerciseData.total}
-   lowerBody = {exerciseData.lowerbody}
-   upperBody = {exerciseData.upperBody} 
-   cardio = {exerciseData.cardio}
-   goal = {exerciseData.goal}/>
-  ,
-  document.getElementById('root')
-)
+ReactDOM.render(<Products />, document.getElementById('root'))
